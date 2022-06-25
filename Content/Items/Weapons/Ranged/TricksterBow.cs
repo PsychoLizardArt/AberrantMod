58a1b7ad790fe61368771a8b0b406aca5872d67b
Mack/Sidney <107726564+PsychoLizardArt@users.noreply.github.com>
2022-06-25 08:52:44 -0400

    Update TricksterBow.cs

--- Content/Items/Weapons/Ranged/TricksterBow.cs
+++ Content/Items/Weapons/Ranged/TricksterBow.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Aberrant.Content.Items.Weapons.Ranged
+namespace AberrantMod.Content.Items.Weapons.Ranged
 {
     public class TricksterBow : ModItem
     {
