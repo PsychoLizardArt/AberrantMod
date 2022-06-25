2cf8a55fdc7f796a10ba95f9dd6d4a5e78ae5557
Mack/Sidney <107726564+PsychoLizardArt@users.noreply.github.com>
2022-06-25 08:51:53 -0400

    Update PentagramRed.cs

--- Content/Items/Projectiles/PentagramRed.cs
+++ Content/Items/Projectiles/PentagramRed.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Aberrant.Content.Items.Projectiles
+namespace AberrantMod.Content.Items.Projectiles
 {
     public class PentagramRed : ModProjectile
     {
