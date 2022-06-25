48a43318d5c8859a22c9d92ece17ae5837168a55
Mack/Sidney <107726564+PsychoLizardArt@users.noreply.github.com>
2022-06-25 08:54:15 -0400

    Update MakeshiftLockpick.cs

--- Content/NPCs/Boss/Trickster/MakeshiftLockpick.cs
+++ Content/NPCs/Boss/Trickster/MakeshiftLockpick.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aberrant.AberrantNPCs.Bosses.TricksterBoss
+namespace AberrantMod.Content.NPCs.Boss.Trickster
 {
     public class MakeshiftLockpick : ModItem
     {
