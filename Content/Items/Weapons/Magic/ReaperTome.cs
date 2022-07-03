0c4794bdbcd8813d82eaa7655266faeeca22da44
Mack/Sidney <psycholizardart@gmail.com>
2022-07-02 21:05:42 -0400

    testing this

---
+++ Content/Items/Weapons/Magic/ReaperTome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+
+namespace AberrantMod.Content.Items.Weapons.Magic
+{
+    public class ReaperTome : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            
+        }
+    }
+}
